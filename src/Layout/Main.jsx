af599d22df1f4481328204e3fa10736f2587346d
renco88 <mdmahadihassanrenco@gmail.com>
2024-12-07 23:33:40 +0600

    first commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Home/Shared/Footer';
+import Navber from '../Pages/Home/Shared/Navber';
+
+const Main = () => {
+    return (
+        <div>
+            <Navber></Navber>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
